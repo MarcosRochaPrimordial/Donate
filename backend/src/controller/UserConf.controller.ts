21aa96e284785d1c4a3a66e970b5b191777fe96f
Thierry-Freire <thierry.freire@hotmail.com>
2018-12-02 15:32:46 -0300

    Criação do método de seguir um usuário

--- backend/src/controller/UserConf.controller.ts
+++ backend/src/controller/UserConf.controller.ts
@@ -3,11 +3,13 @@ import { Request, Response } from 'express';
 
 import { UserModel } from '../model/User.model';
 import { Authentication } from '../config/Authentication';
+import { UserFollowerModel } from '../model/UserFollower.model';
 
 @Controller({ url: '/userconf', auth: Authentication, cors: '*' })
 export class UserConfController {
 
     private _userModel: UserModel;
+    private _userFollowerModel: UserFollowerModel;
 
     constructor() {
         this._userModel = new UserModel();
@@ -23,6 +25,13 @@ export class UserConfController {
     @Put('/changepassword')
     changePassword(req: Request, res: Response): void {
         this._userModel.changePassword(req.body, (response: any, status: number) => {
+            res.status(status).json(response);
+        });
+    }
+
+    @Put('/followuser')
+    followUser(req: Request, res: Response): void {
+        this._userFollowerModel.followUser(req.body, (response: any, status: any) => {
             res.status(status).json(response);
         });
     }
--- backend/src/model/UserFollower.model.ts
+++ backend/src/model/UserFollower.model.ts
@@ -1,9 +1,33 @@
-import { User } from './User';
-import { handleErrorsFromDb } from './../config/DbHandles';
-import * as jwt from 'jsonwebtoken';
+import { UserService } from "../service/User.service";
+
+import { User } from "./User";
+import { handleErrorsFromDb } from "./../config/DbHandles";
+import * as jwt from "jsonwebtoken";
+import { ObjectId } from "bson";
+import UserFollower from './UserFollower';
 
 export class UserFollowerModel {
-    public save(callback) {
-        //Função de seguir um doador na aplicação
-    }
+  public followUser(body, callback) {
+    let user = UserService.getInstance().getUser();
+    let loggedUser_id: ObjectId = user._id;
+    let followedUser_id: ObjectId = body._id;
+
+    UserFollower.findOne({loggedUser_id}, (err: any, data: any) => {
+        if (err) {
+            handleErrorsFromDb(err, callback, 503);
+        } else {
+            UserFollower.update({ _id: loggedUser_id}, {
+                $set: {
+                    userFollowed: data.userFollowed.push(followedUser_id)
+                }
+            }).then(function (err) {
+                if (err) {
+                    handleErrorsFromDb(err, callback, 503);
+                } else {
+                    callback('sucess', 503);
+                }
+            });
+        }
+    });
+  }
 }
