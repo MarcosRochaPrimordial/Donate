cc1dcf05b1cbbcc6233beea3422146edf96c8507
MarcosRochaPrimordial <markossdeath@gmail.com>
2018-12-05 00:16:13 -0300

    adicionado profile

--- Donate/src/app/home/home-routing.module.ts
+++ Donate/src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { Routes, RouterModule } from "@angular/router";
 import { StoriesComponent } from './stories/stories.component';
 import { NgModule } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
     {
         path: '',
         component: StoriesComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent
     }
 ];
 
--- Donate/src/app/home/home.module.ts
+++ Donate/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoriesComponent } from './stories/stories.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [StoriesComponent],
+  declarations: [StoriesComponent, ProfileComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
